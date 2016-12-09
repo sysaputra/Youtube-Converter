1af4ddca3cd0f65dcdebe50941f24ec71ea2ece3
sysaputra <131110685@mhs.stiki.ac.id>
2016-12-09 19:40:27 +0700

    okok

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-<link type="text/css" rel="stylesheet" href="/bootstrap/css/bootstrap.css">
+<link type="text/css" rel="stylesheet" href="/stylesheets/bootstrap.css">
 <script src="/js/bootstrap.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 </head>
